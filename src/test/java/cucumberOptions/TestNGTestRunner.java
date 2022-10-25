a5cee49a39124919cf8cc034554d3be78e4995be
Azima Keshwani <[vazima786@gmail.comgit config --global user.email [azima786@gmail.com]>
2022-10-25 15:47:55 -0500

    final

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -4,9 +4,15 @@ package cucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions
-        (features = "./src/test/java/features",
-                glue= "stepDefinitions")
+@CucumberOptions(
+           features = "./src/test/java/features",
+                   glue= "stepDefinitions",
+                   tags = "@SmokeTest or @RegTest or @MobileTest",
+                   dryRun = false, //if any of test cases is not mapped. It will not run
+                   monochrome = true, //output will be detailed and pretty format
+                   plugin = {"pretty", "html:target/cucumber",
+                   "json:target/cucumber.json",
+                   "junit:target/cakes.xml"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
 
--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -7,7 +7,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "./src/test/java/features",glue= "stepDefinitions", stepNotifications = true, tags = "@SmokeTest or @RegTest or @MobileTest"
+        features = "./src/test/java/features",
+        glue= "stepDefinitions",
+        stepNotifications = true,
+        tags = "@SmokeTest or @RegTest or @MobileTest",
+        dryRun = false, //if any of test cases is not mapped. It will not run
+        monochrome = true, //output will be detailed and pretty format
+        plugin = {"pretty", "html:target/cucumber",
+                "json:target/cucumber.json",
+        "junit:target/cakes.xml"}
 
         //"NOT @SanityTest" - will not run these
 )
